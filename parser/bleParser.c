9041cd6a03a3df7946e014faa49f87d672767e0c
Sylvain Decastel <s.decastel@gmail.com>
2016-04-10 23:11:21 +0200

    Finalize database accesses

--- parser/bleParser.c
+++ parser/bleParser.c
@@ -12,12 +12,21 @@
 #include <stdbool.h>
 #include <math.h>
 
-#include "../common.h"
-#include "../libdb.h"
+#include "../common/common.h"
+#include "../common/libdb.h"
 
 #define ASCII_FRAME_START_CHAR '>'
 
 #define BYTE(b) 0xFF&b
+
+
+typedef struct {
+	char macAddr[20];
+	float temperature;
+	int battery;
+	int rssi;
+}Point_t;
+
 
 int convertASCII2Bin(char* pFrame, uint16_t len)
 {
@@ -74,13 +83,12 @@ float IEEE11073_to_float(uint32_t mantissa, char exponent)
 }
 
 
-int decodeFields(char* pBuffer, uint16_t len)
+int decodeFields(char* pBuffer, uint16_t len, Point_t *pPoint)
 {
 	char startBytes[] = {0x04,0x3E,0x26,0x02,0x01,0x03,0x01};
 	int nbServices;
-	char mac[6];
 	char *p;
-	
+	char mac[6];	
 
 	// we are looking for a specific length
 	if (len != 41)
@@ -90,13 +98,17 @@ int decodeFields(char* pBuffer, uint16_t len)
 	if (memcmp(pBuffer,startBytes, sizeof(startBytes)) != 0)
 		return -2;
 
+
+	memset(pPoint, 0, sizeof(Point_t));
+
+
 	// get the number of services
 	nbServices = pBuffer[5];
 //	printf("Nb services : %u\r\n", nbServices);
 
 	// decode the MAC address
 	memcpy(mac, &pBuffer[7], sizeof(mac));
-//	printf("MAC:%02X:%02X:%02X:%02X:%02X:%02X\r\n",BYTE(mac[0]),BYTE(mac[1]),BYTE(mac[2]),BYTE(mac[3]),BYTE(mac[4]),BYTE(mac[5]));
+	sprintf(pPoint->macAddr, "%02X:%02X:%02X:%02X:%02X:%02X",BYTE(mac[0]),BYTE(mac[1]),BYTE(mac[2]),BYTE(mac[3]),BYTE(mac[4]),BYTE(mac[5]));
 
 	// advertisement type not checked
 
@@ -122,11 +134,13 @@ int decodeFields(char* pBuffer, uint16_t len)
 			uint32_t mantissa = ((uint32_t)p[6]<<16) & 0x00FF0000;
 			mantissa += ((uint32_t)p[5]<<8) & 0x0000FF00;
 			mantissa += (p[4]) & 0x000000FF;
-//			printf("Temperature :(0x%06X)  (%u)^(%d) =  %f\r\n",mantissa, mantissa, exponent, IEEE11073_to_float(mantissa, exponent));
+			pPoint->temperature = IEEE11073_to_float(mantissa, exponent);
+//			printf("Temperature :(0x%06X)  (%u)^(%d) =  %f\r\n",mantissa, mantissa, exponent, pPoint->temperature);
 		}break;
 
 		case 0x0F18:
 //			printf("Battery level : %u\r\n", p[4]);
+			pPoint->battery = p[4];
 		break;
 
 		}
@@ -137,8 +151,8 @@ int decodeFields(char* pBuffer, uint16_t len)
 
 	// decode the RSSI
 	{
-		int32_t rssi = -1 & pBuffer[len-1];
-//		printf("RSSI : (%02X) %d\r\n", rssi, rssi); 
+		pPoint->rssi = -1 & pBuffer[len-1];
+//		printf("RSSI : (%02X) %d\r\n", pPoint->rssi, pPoint->rssi); 
 	}
 
 	return 0;
@@ -150,13 +164,21 @@ int main(void)
 	char buffer[255];
 	ssize_t ret;
 	ssize_t n;
+	DB_Handle_t db;
+	Point_t point;
 
-
-	printf("\r\nBLE parser V0.1 started");
+	printf("\r\nBLE parser V0.2 started");
 
 	printf("\r\nWaiting for incoming data");
 
 	setvbuf(stdin, NULL, _IONBF, 0);
+
+	memset(&db, 0, sizeof(db));
+
+	if (db_open(&db)) {
+		printf("\r\n\nFailed to open db");
+		return -1;
+	}
 
 	do {
 		ret = read(fileno(stdin), buffer, sizeof(buffer));
@@ -166,11 +188,16 @@ int main(void)
 		printf("\r\nbuffer (%u) : %s\r\n", ret, buffer);
 
 		n = convertASCII2Bin(buffer, ret);
-		decodeFields(buffer, n);
+		decodeFields(buffer, n, &point);
+
+		if (db_checkNodeAddr(&db, point.macAddr)) {
+			db_addPoint(&db, point.macAddr, point.temperature, point.battery, point.rssi);
+		}
 
 		fflush(stdout);
 	}while(ret >= 0);
 
+	db_close(&db);
 
         return(0);
 }
